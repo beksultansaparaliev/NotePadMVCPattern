5708b26d6e837ecd9ea5a103e1b0f54059d34894
Beksultan <albinblack7@gmail.com>
2022-07-21 01:15:15 +0300

    Initial Commit

---
+++ NotePadMVCPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotePadMVCPattern
+//
+//  Created by Masaie on 21/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
